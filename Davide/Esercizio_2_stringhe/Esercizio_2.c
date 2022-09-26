adcdd3f5a78361b50d8d3612389159fab8df8786
DavideCerisola <davi.cerisola@gmail.com>
2022-09-26 22:32:01 +0100

    Inizio es. 2 stringhe

---
+++ Davide/Esercizio_2_stringhe/Esercizio_2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    char s[100];
+    long n;
+    long vet[50];
+    int pos_vet=0;
+    while(fgets(s, sizeof(s), stdin)!='\0')
+    {
+        if(strtol(s, null, 10)%2==1)
+        {
+            printf("%l", n);
+        }
+        else
+        {
+            vet[pos_vet] = 
+            pos_vet++;
+        }
+    }
+    
+    return 0;
+
+}
