d0f9d1d9e409acbfca092720618bda8076596132
DavideCerisola <davi.cerisola@gmail.com>
2022-09-28 11:19:17 +0100

    ho modicatoil file

--- Davide/Esercizio_2_stringhe/Esercizio_2.c
+++ Davide/Esercizio_2_stringhe/Esercizio_2.c
@@ -2,11 +2,24 @@
 
 int main()
 {
+
+    /*
+        -ricezione della stringa da input
+        -riconoscimento numeri e salvataggio in un vettore
+        -rilettura del vettore e stampa dei numeri dispari
+        -rilettura del vettore e stampa dei numeri pari
+
+        VERSIONE ALTERNATIVA
+        -ricezione della stringa numero per numero
+        -
+    */
+
     char s[100];
     long n;
     long vet[50];
     int pos_vet=0;
-    while(fgets(s, sizeof(s), stdin)!='\0')
+    fgets(s, sizeof(s), stdin);
+    while(s!='\0')
     {
         if(strtol(s, null, 10)%2==1)
         {
@@ -17,6 +30,7 @@ int main()
             vet[pos_vet] = 
             pos_vet++;
         }
+        fgets(s, sizeof(s), stdin);
     }
     
     return 0;
