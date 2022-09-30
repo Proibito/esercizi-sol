2639f58ca7a5c53be7e9ae812417f6ad1c0fc55d
Proibito04 <edoardobalzano01@gmail.com>
2022-09-30 15:29:19 +0200

    aggiunti esercizi

--- edoardo/esercizi1/es-sum-next.c
+++ edoardo/esercizi1/es-sum-next.c
@@ -7,14 +7,14 @@ int main()
 {
 
     int v2[10] = {0};
-    int v1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
+    int v1[10] = {0};
 
     for (int i = 0; i < 10; i++)
     {
-        printf("v1 %d \n", v1[i]);
+        char carattere[10];
+        fgets(carattere, sizeof(v1), stdin);
+        v1[i] = strtol(carattere, NULL, 10);
     }
-
 
     for (int i = 0; i < 10; i++)
     {
