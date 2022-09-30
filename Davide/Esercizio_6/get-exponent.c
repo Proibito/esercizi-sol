f3fca1b9249b5c49fcc307ccae71b3e774d3884b
DavideCerisola <davide.cerisola@edu.unito.it>
2022-09-30 17:39:27 +0200

    NEW Inizio esercizio Esponente

---
+++ Davide/Esercizio_6/get-exponent.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    double n;
+    int i;
+    double esponente;
+    printf("Inserisci il numero con la virgola: ");
+    scanf("%lf", &n);
+
+    esponente = (unsigned)n|(unsigned)2146435072;      //numero ricavato dalla disposizione dei bit che permette di avere il bit più significativo a zero, 11 uno e il resto zero.
+
+    printf("Esponente = %lf\n", esponente);
+
+    return 0;
+}
