f96a2b08a72bac3853c94d66c2b00ec5ce9565a7
DavideCerisola <davide.cerisola@edu.unito.it>
2022-10-05 11:04:47 +0200

    Cambiamenti minori esercizio esponenti

--- Davide/Esercizio_6/get-exponent.c
+++ Davide/Esercizio_6/get-exponent.c
@@ -15,14 +15,15 @@ int main()
     int p_esponente;
     int esponente=0;
     int intero=0;
+    int esponente_dec;
     printf("Inserisci il numero con la virgola: ");
     scanf("%lf", &n);
 
-    memcpy(&intero, ((char*)&n+6), 2);
-    printf("Intero = %4x\n", intero);
-    intero &= 0x7FFF;
-    intero = intero>>4;
-    printf("Intero (dopo shift) = %4x\n", intero);
+    memcpy(&intero, ((char*)&n+6), 2);      //copio i bit della porzione di memoria che mi interessa e li metto nella variabile intero.
+    printf("Intero = %4x\n", intero);       //stampo intero prima di aver rimosso i bit che non mi interessano
+    intero &= 0x7FFF;                       //uso una maschera per rimuovere il primo bit del segno
+    intero = intero>>4;                     //shift a destra di 4 posizioni in modo da eliminare i bit della mantissa rimanenti
+    printf("Intero (dopo shift) = %4x\ndecimale -> %d", intero, intero-1023);  //stampa
 
     /*esponente = *((char*)&n+6);
     esponente = esponente<<2;
@@ -31,7 +32,7 @@ int main()
     //dovrebbe adesso puntare alla zona dell'esponente
 
 
-    printf("Esponente = %d\tIndirizzo = %p", esponente, &esponente);
+    //printf("Esponente = %d\tIndirizzo = %p", esponente, &esponente);
 
     //printf("\nINDIRIZZI\nposizione = %p\nesponente = %p\nesponente+8*8 = %p\n\nCONTENUTI\n(pre-modifica)\t esponente = %li", &n+1, &esponente, &esponente+8*8, esponente);
     //memcpy(&esponente+8*8, &n+1, 11);
@@ -40,6 +41,9 @@ int main()
     //printf("Esponente = %lf\n", esponente);
 
     printf("\nEsponente = %d\n", esponente);
+    //Elaboro esponente per renderlo decimale
+
+    //esponente_dec=esponente_dec-1024;
 
     return 0;
 }
