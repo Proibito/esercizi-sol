c04f3a459aa962493cd502f4afd86f39221d60d2
DavideCerisola <davide.cerisola@edu.unito.it>
2022-10-05 14:18:28 +0200

    Micro-cambiamenti

--- Davide/Esercizio_7/binary.c
+++ Davide/Esercizio_7/binary.c
@@ -4,12 +4,13 @@
 int main()
 {
     char stringa_input[100];
+    int i;
     fgets(stringa_input, sizeof(stringa_input), stdin);
     int esponente=0;
     while(stringa_input[0]!='\0')
     {
         
-        for(int i=0;i<strlen(stringa_input);i=i+4)
+        for(i=0;i<strlen(stringa_input);i=i+4)
         {
             stringa_input[i]>>4;
         }
