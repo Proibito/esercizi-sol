b40cc646eb05791e84ad7b82e5c1382bce48d3a1
DavideCerisola <davide.cerisola@edu.unito.it>
2022-10-05 14:05:51 +0200

    Aggiunta PDF e Esercizio-7 funzionante

---
+++ Davide/Esercizio_7/binary.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char stringa_input[100];
+    fgets(stringa_input, sizeof(stringa_input), stdin);
+    int esponente=0;
+    while(stringa_input[0]!='\0')
+    {
+        
+        for(int i=0;i<strlen(stringa_input);i=i+4)
+        {
+            stringa_input[i]>>4;
+        }
+        esponente++;
+    }
+
+    return 0;
+}
