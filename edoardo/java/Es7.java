dbd032c97b608875113d4c7dee230082b76569a5
Proibito04 <edoardobalzano01@gmail.com>
2022-10-14 15:25:50 +0200

    fatto esercizio 7

---
+++ edoardo/java/Es7.java
@@ -0,0 +1,30 @@
+public class Es7 {
+    public static boolean scan(String s, String nome) {
+        int state = 0;
+        int i = 0;
+        while (state >= 0 && i < s.length()) {
+            int j = i;
+            final char ch = s.charAt(i++);
+            final char chN = nome.charAt(j++);
+            switch (state) {
+                case 0:
+                    if (ch == chN)
+                        state = 0;
+                    else
+                        state = 1;
+                    break;
+                case 1:
+                    if (ch == chN)
+                        state = 1;
+                    else
+                        state = -1;
+                    break;
+            }
+        }
+        return state == 0 || state == 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(scan("edoaldo", "edoardo") ? "ok" : "nope");
+    }
+}
